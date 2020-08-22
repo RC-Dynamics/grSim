8bcedd05771224295b18acc04b24bd49a4e126f1
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2020-08-22 15:39:39 -0300

    sync with python and 120 fps

--- src/configwidget.cpp
+++ src/configwidget.cpp
@@ -96,9 +96,9 @@ ConfigWidget::ConfigWidget()
   world.push_back(phys_vars);
     VarListPtr worldp_vars(new VarList("World"));
     phys_vars->addChild(worldp_vars);  
-        ADD_VALUE(worldp_vars,Double,DesiredFPS,500,"Desired FPS")
+        ADD_VALUE(worldp_vars,Double,DesiredFPS,120,"Desired FPS")
         ADD_VALUE(worldp_vars,Bool,SyncWithGL,false,"Synchronize ODE with OpenGL")
-        ADD_VALUE(worldp_vars, Bool, SyncWithPython, false, "Synchronize SimStep with python " )
+        ADD_VALUE(worldp_vars, Bool, SyncWithPython, true, "Synchronize SimStep with python " )
         ADD_VALUE(worldp_vars,Double,DeltaTime,0.03,"ODE time step")
         ADD_VALUE(worldp_vars,Double,Gravity,9.8,"Gravity")
         ADD_VALUE(worldp_vars,Bool,ResetTurnOver,true,"Auto reset turn-over")
